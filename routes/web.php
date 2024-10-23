d71e1f6afd1350b570de62602a577c28486bb6d4
malikah <siswiian@gmail.com>
2024-10-23 12:39:16 -0600

    route pinjam ruangan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DataJurusanController;
+use App\Http\Controllers\PinjamRuanganController;
 use App\Http\Controllers\RuanganController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,6 @@ Route::resource('admin/ruangan', RuanganController::class);
 
 // data jurusan
 Route::get('admin/data_jurusan', [DataJurusanController::class,'index'])->name('data_jurusan.index');
+
+//pinjam ruangan
+Route::resource('admin/pinjam-ruangan', PinjamRuanganController::class);
